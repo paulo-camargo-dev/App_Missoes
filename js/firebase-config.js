2c794e0a7dc55000a0554b0fafe3c797771ee9ef
Paulo Camargo <p.camargo1508@gmail.com>
2026-03-04 19:12:37 -0300

    Refactor Firebase config and initialization
    Updated Firebase configuration and initialization to use new variables.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,18 +2,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Configuração do seu projeto
+// Config do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "appmissoes-21c4f.firebaseapp.com",
-  projectId: "appmissoes-21c4f",
-  storageBucket: "appmissoes-21c4f.appspot.com",
-  messagingSenderId: "97813114592",
-  appId: "1:97813114592:web:5d51ae9b0bf9f01058ea85"
+    apiKey: "SUA_KEY",
+    authDomain: "SEU_AUTH",
+    projectId: "SEU_PROJECT",
+    storageBucket: "SEU_BUCKET",
+    messagingSenderId: "SEU_SENDER",
+    appId: "SEU_APP"
 };
 
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Inicializar Firestore
-export const db = getFirestore(app);
+// Globais
+const db = firebase.firestore();
+const auth = firebase.auth();
